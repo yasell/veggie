32704078d975f52d74c00ca36afcff64753050a6
yasell <strogofcdm@gmail.com>
2016-05-05 22:25:56 +0300

    common styles

---
+++ README.md
@@ -0,0 +1,9 @@
+# Veggie - Onepage PSD Template.
+
+---
+
+Veggie PSD Template available on www.behance.net/annadadej is royalty free for use in both personal and commercial projects.
+
+---
+
+* Ссылочка Designer: [AnnaDadej](https://www.behance.net/annadadej).
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+      ]}),
+      mqpacker({
+        sort: true
+      })
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.reload({stream: true}));
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
